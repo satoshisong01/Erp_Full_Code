2c14ab59743675e0db05c2ebe790608d59deb8aa
yoo <s_plus7@naver.com>
2023-04-27 17:52:52 +0900

    login UI 수정

--- src/views/auth/components/Login.js
+++ src/views/auth/components/Login.js
@@ -2,197 +2,171 @@ import React from "react";
 
 import UiValidate from "../../../common/forms/validation/UiValidate";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default class Login extends React.Component {
-  onClick = e => {
-    e.preventDefault();
-  };
-  render() {
-    return (
-      <div id="extr-page">
-        <header id="header" className="animated fadeInDown">
-          <div id="logo-group">
-            <span id="logo">
-              {" "}
-              <img src="assets/img/logo.png" alt="SmartAdmin" />{" "}
-            </span>
-          </div>
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: '',
+			password: '',
+			userSe: 'USR'
+		};
+	}
 
-          <span id="extr-page-header-space">
-            {" "}
-            <span className="hidden-mobile hiddex-xs">Need an account?</span>
-            &nbsp;
-            <a href="#/register" className="btn btn-danger">
-              Create account
-            </a>{" "}
-          </span>
-        </header>
-        <div id="main" role="main" className="animated fadeInDown">
-          <div id="content" className="container">
-            <div className="row">
-              <div className="col-xs-12 col-sm-12 col-md-7 col-lg-8 hidden-xs hidden-sm">
-                <h1 className="txt-color-red login-header-big">SmartAdmin</h1>
+	handleSubmit(e) {
+		let userInfo = {
+			id: this.state.id,
+			password: this.state.password,
+			userSe: this.state.userSe,
+		}
 
-                <div className="hero">
-                  <div className="pull-left login-desc-box-l">
-                    <h4 className="paragraph-header">
-                      It's Okay to be Smart. Experience the simplicity of
-                      SmartAdmin, everywhere you go!
-                    </h4>
+		// const SERVER_URL = "http://192.168.0.123:8080" //전역변수로 바꾸기
+		const SERVER_URL = "http://localhost:8080"
+		const loginUrl = "/uat/uia/actionLoginJWT.do"
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify(userInfo)
+		}
 
-                    <div className="login-app-icons">
-                      <a
-                        href="#/dashboard/analytics"
-                        className="btn btn-danger btn-sm"
-                      >
-                        Frontend Template
-                      </a>
-                      <span> </span>
-                      <a
-                        href="#/smartadmin/different-versions.html"
-                        className="btn btn-danger btn-sm"
-                      >
-                        Find out more
-                      </a>
-                    </div>
-                  </div>
-                  <img
-                    src="assets/img/demo/iphoneview.png"
-                    className="pull-right display-image"
-                    alt=""
-                    style={{ width: "210px" }}
-                  />
-                </div>
-                <div className="row">
-                  <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                    <h5 className="about-heading">
-                      About SmartAdmin - Are you up to date?
-                    </h5>
+		console.log(" userInfo >>>> ", userInfo);
+		fetch(SERVER_URL + loginUrl, requestOptions)
+			.then(res => {
+				return res.json();
+			})
+			.then((resp) => {
+				if (Number(resp.resultCode) === 200) {
+					//alert("Login Alert"); //라우터파일에 /admin/으로 시작하는 URL은 모두 인증없이 접근 할 때 경고창이 나오도록 조치했기 때문에 제외했음.
+					// sessionStorage.setItem('loginUser', JSON.stringify({"id":""}));
+					// window.location.href = URL.LOGIN;
+					return false;
+				} else {
+					return resp;
+				}
+			})
+			.catch(error => {
+				console.error("login error!", error);
+			})
 
-                    <p>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa.
-                    </p>
-                  </div>
-                  <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                    <h5 className="about-heading">
-                      Not just your average template!
-                    </h5>
+	};
 
-                    <p>
-                      Et harum quidem rerum facilis est et expedita distinctio.
-                      Nam libero tempore, cum soluta nobis est eligendi
-                      voluptatem accusantium!
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-5 col-lg-4">
-                <div className="well no-padding">
-                  <UiValidate>
-                    <form
-                      action="#/dashboard"
-                      id="login-form"
-                      className="smart-form client-form"
-                    >
-                      <header>Sign In</header>
-                      <fieldset>
-                        <section>
-                          <label className="label">E-mail</label>
-                          <label className="input">
-                            {" "}
-                            <i className="icon-append fa fa-user" />
-                            <input
-                              type="email"
-                              name="email"
-                              data-smart-validate-input=""
-                              data-required=""
-                              data-email=""
-                              data-message-required="Please enter your email address"
-                              data-message-email="Please enter a VALID email address"
-                            />
-                            <b className="tooltip tooltip-top-right">
-                              <i className="fa fa-user txt-color-teal" />
-                              Please enter email address/username
-                            </b>
-                          </label>
-                        </section>
-                        <section>
-                          <label className="label">Password</label>
-                          <label className="input">
-                            {" "}
-                            <i className="icon-append fa fa-lock" />
-                            <input
-                              type="password"
-                              name="password"
-                              data-smart-validate-input=""
-                              data-required=""
-                              data-minlength="3"
-                              data-maxnlength="20"
-                              data-message="Please enter your email password"
-                            />
-                            <b className="tooltip tooltip-top-right">
-                              <i className="fa fa-lock txt-color-teal" /> Enter
-                              your password
-                            </b>{" "}
-                          </label>
+	render() {
+		return (
+			<div id="extr-page">
+				<header id="header" className="animated fadeInDown">
+					<div id="logo-group">
+						<span id="logo">
+							{" "}
+							<img src="assets/img/logo.png" alt="SmartAdmin" />{" "}
+						</span>
+					</div>
 
-                          <div className="note">
-                            <a href="#/forgot">Forgot password?</a>
-                          </div>
-                        </section>
-                        <section>
-                          <label className="checkbox">
-                            <input
-                              type="checkbox"
-                              name="remember"
-                              defaultChecked={true}
-                            />
-                            <i />
-                            Stay signed in
-                          </label>
-                        </section>
-                      </fieldset>
-                      <footer>
-                        <button type="submit" className="btn btn-primary">
-                          Sign in
-                        </button>
-                      </footer>
-                    </form>
-                  </UiValidate>
-                </div>
-                <h5 className="text-center"> - Or sign in using -</h5>
-                <ul className="list-inline text-center">
-                  <li>
-                    <a href="#/"
-                      onClick={this.onClick}
-                      className="btn btn-primary btn-circle"
-                    >
-                      <i className="fa fa-facebook" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#/"
-                      onClick={this.onClick}
-                      className="btn btn-info btn-circle"
-                    >
-                      <i className="fa fa-twitter" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#/"
-                      onClick={this.onClick}
-                      className="btn btn-warning btn-circle"
-                    >
-                      <i className="fa fa-linkedin" />
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+					<span id="extr-page-header-space">
+						{" "}
+						<span className="hidden-mobile hiddex-xs">Need an account?</span>
+						&nbsp;
+						<a href="#/register" className="btn btn-danger">
+							Create account
+						</a>{" "}
+					</span>
+				</header>
+				<div id="main" role="main" className="animated fadeInDown">
+					<div id="content" className="container">
+						<div className="row">
+							<div className="col-xs-12 col-sm-12 col-md-5 col-lg-5" style={{ margin: "0 auto", float: 'unset'}}>
+								<div className="well no-padding">
+									<UiValidate>
+										<form
+											// action="#/dashboard"
+											action="#/tables/jquery-tables"
+											id="login-form"
+											className="smart-form client-form"
+											onSubmit={this.handleSubmit.bind(this)}
+										>
+											<header>Sign In</header>
+											<fieldset>
+												<section>
+													<label className="label">아이디</label>
+													<label className="input">
+														{" "}
+														<i className="icon-append fa fa-user" />
+														<input
+															type="text"
+															name="loginId"
+															data-smart-validate-input=""
+															data-required=""
+															data-message-required="Please enter your id"
+															data-message-id="Please enter a VALID id" //형식이 틀릴때 나오는 메세지
+															onChange={(e) => this.setState({ id: e.target.value})}
+														/>
+														{/* tooltip 안됨 */}
+														<b className="tooltip tooltip-top-right">
+															<i className="fa fa-user txt-color-teal" />
+															Please enter id/username
+														</b>
+													</label>
+												</section>
+												<section>
+													<label className="label">비밀번호</label>
+													<label className="input">
+														{" "}
+														<i className="icon-append fa fa-lock" />
+														<input
+															type="password"
+															name="password"
+															data-smart-validate-input=""
+															data-required=""
+															data-minlength="3"
+															data-maxnlength="20"
+															data-message="Please enter your password"
+															onChange={(e) => this.setState({ password : e.target.value })}
+														/>
+														<b className="tooltip tooltip-top-right">
+															<i className="fa fa-lock txt-color-teal" />
+															Enter your password
+														</b>{" "}
+													</label>
+
+												</section>
+												<section style={{ display: "flex", width: "100%" }}>
+												{/* <section> */}
+													{/* <div className="note"> */}
+													<div className="" style={{flex: 1}}>
+														<a href="#/forgot">Forgot password?</a>
+													</div>
+													<div>
+														<label className="checkbox" style={{ border: "none", width: "100%", margin: 0, float: "left", flex: 1}}>
+															<input
+															type="checkbox"
+															name="remember"
+															defaultChecked={true}
+														/>
+															<i /> Remember me
+														</label>
+													</div>
+												</section>
+											</fieldset>
+											<footer>
+												<button type="submit" className="btn btn-primary">
+												Sign in
+												</button>
+											</footer>
+										</form>
+									</UiValidate>
+								</div>
+							</div>
+						{/* row */}
+						</div>
+						{/* container */}
+					</div>
+				{/* main */}
+				</div>
+			{/* extr-page */}
+			</div>
+			
+		);
+	}
 }
