9ff082aa4ea374c481fe7605438c995d2a0fdaf5
yoo <s_plus7@naver.com>
2023-08-30 13:28:48 +0900

    메인 화면 변경

--- src/pages/main/EgovMain.jsx
+++ src/pages/main/EgovMain.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Link, useLocation } from "react-router-dom";
-
-import * as EgovNet from "api/egovFetch";
-import URL from "constants/url";
-import PopupButton from "components/button/PopupButton";
-import CategoryCode from "pages/system/CategoryCode";
+import { useLocation } from "react-router-dom";
+import ReactDataTableRowEdit from "components/DataTable/ReactDataTableRowEdit";
 
 function EgovMain(props) {
     console.group("EgovMain");
@@ -14,60 +10,14 @@ function EgovMain(props) {
     const location = useLocation();
     console.log("EgovMain [location] : ", location);
 
-    const popupBtnData1 = {title: '인건비 계획', btnName: '계획등록'}
-    const popupBtnData2 = {title: '사전 원가 계산서', btnName: '사전원가서'}
-
     console.log("------------------------------EgovMain [End]");
-    console.groupEnd("EgovMain");
+    console.groupEnd("EgovMain")
 
     return (
-        <div className="egov-container P_MAIN">
+        <div className="egov-container">
             <div className="c_wrap">
-                <div className="mini_board">
-                    <div className="col" style={{ height: "100%" }}>
-                        <PopupButton
-                            targetUrl={URL.ApprovalContainer}
-                            data={popupBtnData1}
-                        />
-                        <PopupButton
-                            targetUrl={URL.CostStatement}
-                            data={popupBtnData2}
-                        />
-                    </div>
-
-                    <div className="col">
-                        <CategoryCode />
-                    </div>
-
-                    <div className="col">
-                        <ul className="tab">
-                            <li>
-                                <a href="#공지사항" className="on">
-                                    공지사항
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#갤러리">갤러리</a>
-                            </li>
-                        </ul>
-                        <div className="list">
-                            <div className="notice">
-                                <h2 className="blind">공지사항</h2>
-                                <ul> 공지사항 </ul>
-                                <Link to={URL.INFORM_NOTICE} className="more">
-                                    더보기
-                                </Link>
-                            </div>
-
-                            <div className="gallary">
-                                <h2 className="blind">갤러리</h2>
-                                <ul> 갤러리 </ul>
-                                <Link to={URL.INFORM_GALLERY} className="more">
-                                    더보기
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
+                <div className="logo-center-container">
+                    <img src="assets/images/mecca_logo.png" alt="로고" className="logo-main" />
                 </div>
             </div>
         </div>
