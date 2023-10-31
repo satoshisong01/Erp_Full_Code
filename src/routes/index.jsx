aa2d6e0c97e3a87ec4e1635dccd5728a3e06f240
송경석 <sks8982@gmail.com>
2023-10-31 17:43:22 +0900

    fnAddPgNm,fnAddPdiNm 빈값 추가 방지

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -167,8 +167,12 @@ const RootRoutes = () => {
     };
 
     useEffect(() => {
-        fnAddPgNm();
-        fnAddPdiNm();
+        if (addPgNm === true) {
+            fnAddPgNm();
+        }
+        if (addPdiNm === true) {
+            fnAddPdiNm();
+        }
     }, [addPgNm, addPdiNm]);
 
     const fnAddPgNm = async () => {
