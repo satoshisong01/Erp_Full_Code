badb6b73298a47455db70fc430f313ed06d4ef89
송경석 <sks8982@gmail.com>
2023-12-08 09:38:44 +0900

    데이터추가 하던코드 (프로젝트 추가)

--- src/components/modal/AddModal.jsx
+++ src/components/modal/AddModal.jsx
@@ -1,15 +1,41 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "../../components/modal/ModalSearch.css";
 import "datatables.net-dt/css/jquery.dataTables.css";
 import "datatables.net-dt/js/dataTables.dataTables";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { v4 as uuidv4 } from "uuid";
+import DayPicker from "components/input/DayPicker";
+import { PageContext } from "components/PageProvider";
+import ModalPageCompany from "./ModalPageCompany";
 
 /* 추가 모달 */
 export default function AddModal(props) {
-    const {width, height, columns, onClose, sendData} = props;
+    const { width, height, columns, onClose, sendData, errorOn } = props;
+    const [showAlert, setShowAlert] = useState(false);
+    const [errorOnState, setErrorOnState] = useState(false);
+    const { isOpenModalCompany, setIsOpenModalCompany } = useContext(PageContext);
+
     const [data, setData] = useState({});
+
+    const handleDateClick = (date, colName, index) => {
+        console.log(date, colName, index);
+        setData((prevData) => ({
+            ...prevData,
+            [colName]: date,
+        }));
+    };
+
+    const handleCompany = (date) => {
+        setData((prevData) => ({
+            ...prevData,
+            //[colName]: date,
+        }));
+    };
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
 
     const inputChange = (e) => {
         const { name, value } = e.target;
@@ -19,16 +45,31 @@ export default function AddModal(props) {
         }));
     };
 
+    //useEffect(() => {
+    //    setErrorOnState(errorOn); // Update errorOnState when errorOn changes
+    //}, [errorOn]);
+
     // 데이터 추가 버튼을 눌렀을 때 실행되는 함수
     const onAdd = async (e) => {
         e.preventDefault();
-
+        console.log("문제없으면 통과");
+        sendData(data);
         // 필수 필드가 비어있는지 확인
-        const requiredColumns = columns.filter((column) => column.require);
-        const hasEmptyRequiredFields = requiredColumns.some((column) => !data[column.col]);
-        //필수값 확인 후
-        sendData(); //데이터 부모로 전송
+        //const requiredColumns = columns.filter((column) => column.require);
+        //const hasEmptyRequiredFields = requiredColumns.some((column) => !data[column.col]);
+        ////필수값 확인 후
+        //if (hasEmptyRequiredFields) {
+        //    setShowAlert(true); // 알림 메시지 표시
+        //} else {
+        //    sendData(data); // 데이터 추가 함수 호출
+        //}
         onClose();
+    };
+
+    const setValueCompany = () => {
+        //setRowIndex()
+        setIsOpenModalCompany(true);
+        //setRowIndex(rowIndex);
     };
 
     return (
@@ -47,64 +88,105 @@ export default function AddModal(props) {
                             </div>
                         </div>
                         <form className="product-modal-body">
-                            <div className="submitProduct">
-                                {columns && columns.map((column, index) => {
+                            {columns.map((column, index) => {
+                                if (column.add) {
                                     return (
-                                        <div className="postBox" key={index}>
-                                            <div className="inputBox">
-                                                <label className="postLabel">
-                                                    {column.require && <span className="redStar">*</span>}
-                                                    {column.header}:
-                                                </label>
-                                                {column.type === "select" ? (
-                                                    <select
-                                                        key={uuidv4()}
-                                                        name={column.col}
-                                                        className="postInput"
-                                                        onChange={inputChange}
-                                                    >
-                                                        {column.option.map((op) => (
-                                                            <option key={uuidv4()} value={op.value}>
-                                                                {op.label}
-                                                            </option>
-                                                        ))}
-                                                    </select>
-                                                ) : (
-                                                    <input
-                                                        key={uuidv4()}
-                                                        placeholder={column.placeholder || column.header}
-                                                        className="postInput"
-                                                        type="text"
-                                                        name={column.col}
-                                                        value={data[column.col] || ""}
-                                                        onChange={inputChange}
-                                                    />
+                                        <div className="submitProduct" key={index}>
+                                            {/* 첫 번째 postBox */}
+                                            <div className="postBox">
+                                                <div className="inputBox">
+                                                    <label className="postLabel">
+                                                        {column.require && <span className="redStar">*</span>}
+                                                        {column.header}:
+                                                    </label>
+                                                    {column.type === "select" ? (
+                                                        <select name={column.col} className="postInput" onChange={inputChange}>
+                                                            {column.option.map((op) => (
+                                                                <option key={op.value} value={op.value}>
+                                                                    {op.label}
+                                                                </option>
+                                                            ))}
+                                                        </select>
+                                                    ) : column.lockAt ? (
+                                                        <select name={column.col} className="postInput" onChange={inputChange}>
+                                                            <option value="Y">Y</option>
+                                                            <option value="N">N</option>
+                                                        </select>
+                                                    ) : column.itemType && Array.isArray(column.itemType) ? (
+                                                        <select className="postInput" name={column.col} value={data[column.col] || ""} onChange={inputChange}>
+                                                            <option value={""}>{column.itemType[0]}</option>
+                                                            {column.itemType.map(
+                                                                (item, index) =>
+                                                                    index > 0 && (
+                                                                        <option key={index} value={column.itemTypeSymbol[index]}>
+                                                                            {item}
+                                                                        </option>
+                                                                    )
+                                                            )}
+                                                        </select>
+                                                    ) : column.type === "datepicker" ? (
+                                                        <DayPicker
+                                                            name={column.col}
+                                                            value={data[column.col] || ""}
+                                                            onClick={(data) => handleDateClick(data, column.col, index)}
+                                                        />
+                                                    ) : column.type === "buttonCompany" ? (
+                                                        <div>
+                                                            <input
+                                                                className="buttonSelect"
+                                                                id={column.col}
+                                                                name={column.col}
+                                                                onClick={() => setValueCompany()}
+                                                                type="text"
+                                                                placeholder={`거래처명을 선택해 주세요.`}
+                                                                value={data[column.col] || ""}
+                                                                //onChange={(data) => handleCompany(data)}
+                                                                readOnly
+                                                            />
+                                                        </div>
+                                                    ) : (
+                                                        <input
+                                                            placeholder={column.placeholder || column.header}
+                                                            className="postInput"
+                                                            type="text"
+                                                            name={column.col}
+                                                            value={data[column.col] || ""}
+                                                            onChange={inputChange}
+                                                        />
+                                                    )}
+                                                </div>
+                                                {column.require && showAlert && !data[column.col] && (
+                                                    <span className="error-message text-error">필수값이 비어있습니다.</span>
                                                 )}
+                                                {/*{errorOnState && column.pk && <span className="error-message text-error">중복된 값입니다.</span>}
+                                                {!errorOnState && column.pk && <span className="error-message text-error"> </span>}*/}
                                             </div>
                                         </div>
                                     );
-                                })}
-                            </div>
-                            <div className="modal-footer">
-                                <button
-                                    type="button"
-                                    className="btn btn-default"
-                                    data-dismiss="modal"
-                                    onClick={() => { onClose(); }}>
-                                    취소
-                                </button>
-                                <button
-                                    type="button"
-                                    className="btn btn-primary modal-btn-close"
-                                    id="modalSubmitBtn"
-                                    onClick={onAdd}>
-                                    추가
-                                </button>
-                            </div>
+                                }
+                                return null;
+                            })}
                         </form>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-default"
+                                data-dismiss="modal"
+                                onClick={() => {
+                                    //fetchAllData();
+                                    onClose();
+                                    //handleSendLoading(true);
+                                }}>
+                                취소
+                            </button>
+                            <button type="button" className="btn btn-primary modal-btn-close" id="modalSubmitBtn" onClick={onAdd}>
+                                추가
+                            </button>
+                        </div>
                     </div>
                 </article>
             </div>
+            {isOpenModalCompany && <ModalPageCompany onClose={() => setIsOpenModalCompany(false)} />}
         </div>
     );
 }
--- src/pages/sales/OrderMgmt.jsx
+++ src/pages/sales/OrderMgmt.jsx
@@ -11,31 +11,53 @@ import AddButton from "components/button/AddButton";
 import PopupButton from "components/button/PopupButton";
 import URL from "constants/url";
 import AddModal from "components/modal/AddModal";
+import { axiosDelete, axiosFetch, axiosPost, axiosUpdate } from "api/axiosFetch";
+import DeleteModal from "components/modal/DeleteModal";
 
 /** 영업관리-수주등록관리 */
 function OrderMgmt() {
     const { projectInfo } = useContext(PageContext);
     const orderMgmtTable = useRef(null);
     const [isOpenAdd, setIsOpenAdd] = useState(false);
+    const [isOpenUpDate, setIsOpenUpDate] = useState(false);
+    const [isOpenDel, setIsOpenDel] = useState(false);
+
+    const [sendDataTable, setSendDataTable] = useState([]);
+
+    //임시 삭제 할 id,명
+    const [poiId, setPoiId] = useState([]);
+    const [poiNm, setPoiNm] = useState([]);
 
     const addColumns = [
-        { header: "프로젝트이름", col: "poiNm", placeholder: "ㅇㅇ", require: true},
-        { header: "고객사", col: "cltNm"},
-        { header: "수주부서", col: "poiGroupId"},
-        { header: "매출부서", col: "poiSalesGroupId"},
-        { header: "영업대표", col: "poiSalmanagerId"},
-        { header: "PM", col: "poiManagerId"},
-        { header: "통화", col: "poiCurrcy"},
-        { header: "계약일", col: "poiBeginDt"},
-        { header: "납기시작일", col: "poiDueBeginDt"},
-        { header: "납기종료일", col: "poiDueEndDt"},
-        { header: "기준이익률", col: "standardMargin"},
-        { header: "상태", col: "poiStatus"}
-    ]
+        { header: "프로젝트이름", col: "poiNm", cellWidth: "25%", type: "input", enable: true, modify: true, add: true, require: true },
+        { header: "PJ코드(임시필수)", col: "poiCode", cellWidth: "25%", type: "input", enable: true, modify: true, add: true, require: true },
+        //{ header: "고객사", col: "cltNm", cellWidth: "15%", type: "buttonCompany", enable: true, modify: true, add: true, require: true },
+        { header: "수주부서", col: "poiGroupId", cellWidth: "7%", type: "input", enable: true, modify: true, add: true, require: true },
+        { header: "매출부서", col: "poiSalesGroupId", cellWidth: "7%", type: "input", enable: true, modify: true, add: true, require: true },
+        { header: "영업대표", col: "poiSalmanagerId", cellWidth: "10%", type: "input", enable: true, modify: true, add: true, require: true },
+        { header: "PM", col: "poiManagerId", cellWidth: "10%", type: "input", enable: true, modify: true, add: true, require: true },
+        { header: "통화", col: "poiCurrcy", cellWidth: "5%", type: "input", enable: true, modify: true, add: true, require: false },
+        { header: "계약일", col: "poiBeginDt", cellWidth: "10%", type: "datepicker", enable: true, modify: true, add: true, require: false },
+        { header: "납기시작일", col: "poiDueBeginDt", cellWidth: "10%", type: "datepicker", enable: true, modify: true, add: true, require: false },
+        { header: "납기종료일", col: "poiDueEndDt", cellWidth: "10%", type: "datepicker", enable: true, modify: true, add: true, require: false },
+        { header: "기준이익률", col: "standardMargin", cellWidth: "8%", type: "input", enable: true, modify: true, add: true, require: false },
+        {
+            header: "상태",
+            col: "poiStatus",
+            cellWidth: "10%",
+            type: "input",
+            itemType: ["상태를 선택해 주세요", "인벤토리접수", "원가작성중", "견적완료", "계약완료"],
+            itemTypeSymbol: ["", "인벤토리접수", "원가작성중", "견적완료", "계약완료"],
+            enable: true,
+            modify: true,
+            add: true,
+            require: true,
+        },
+    ];
 
     const columns = [
         { header: "수주 아이디", col: "poiId", cellWidth: "5%", type: "input", enable: true, modify: false, add: false, require: true, notView: true },
-        { header: "프로젝트명", col: "poiNm", cellWidth: "25%", type: "input", enable: true, modify: true, add: true, require: true },
+        { header: "프로젝트이름", col: "poiNm", cellWidth: "25%", type: "input", enable: true, modify: true, add: true, require: true },
         { header: "고객사", col: "cltNm", cellWidth: "15%", type: "input", enable: true, modify: true, add: false, require: false },
         { header: "수주부서", col: "poiGroupId", cellWidth: "7%", type: "input", enable: true, modify: true, add: true, require: false },
         { header: "매출부서", col: "poiSalesGroupId", cellWidth: "7%", type: "input", enable: true, modify: true, add: true, require: false },
@@ -82,18 +104,75 @@ function OrderMgmt() {
         },
     ];
 
-    const addToServer = () => {
-        //서버에 저장 구현해주세요^^
-    }
-    const modifyToServer = () => {
-        //서버에 수정 구현해주세요^^
-    }
-    const deleteToServer = () => {
-        //서버에 삭제 구현해주세요^^
-    }
+    const saveIdNm = (poiId, poiNm) => {
+        console.log(poiId, poiNm);
+        setPoiId(poiId);
+        setPoiNm(poiNm);
+    };
+
+    const addToServer = async (addData) => {
+        console.log(addData);
+        const url = `/api/baseInfrm/product/pjOrdrInfo/add.do`;
+        const dataToSend = {
+            ...addData,
+            lockAt: "Y",
+            useAt: "Y",
+            deleteAt: "N",
+            poiId: projectInfo.poiId,
+        };
+        const resultData = await axiosPost(url, dataToSend);
+        console.log(resultData);
+        if (resultData) {
+            alert("추가되었습니다");
+            refresh();
+        } else {
+            alert("error!");
+        }
+    };
+    const modifyToServer = async (updatedData) => {
+        const url = `/api/baseInfrm/product/pjOrdrInfo/edit.do`;
+        const updated = { ...updatedData, lockAt: "Y", useAt: "Y" };
+        const resultData = await axiosUpdate(url, updated);
+        console.log(resultData);
+        if (resultData) {
+            alert("수정되었습니다");
+            refresh();
+        } else {
+            alert("error!!");
+        }
+    };
+    const deleteToServer = async () => {
+        // 확인 대화상자 표시
+        const shouldDelete = window.confirm(`🔥${poiNm}🔥 프로젝트를 정말로 삭제하시겠습니까?`);
+        if (shouldDelete) {
+            // 사용자가 "확인"을 클릭하면 삭제 진행
+            const url = `/api/baseInfrm/product/pjOrdrInfo/removeAll.do`;
+            const resultData = await axiosDelete(url, poiId);
+
+            // 필요한 경우 결과 처리
+            if (resultData) {
+                alert(`${poiNm}이(가) 삭제되었습니다.`);
+                // 성공적인 삭제 후 추가 작업 수행
+                refresh();
+            } else {
+                alert(`${poiNm} 삭제 중 오류가 발생했습니다.`);
+            }
+        } else {
+            // 사용자가 "취소"를 클릭하면 아무 작업도 수행하지 않음
+            alert(`${poiNm} 삭제가 취소되었습니다.`);
+        }
+    };
+
     const refresh = () => {
-        //새로고침 구현해주세요^^
-    }
+        fetchAllData();
+    };
+
+    const fetchAllData = async () => {
+        const url = `/api/baseInfrm/product/pjOrdrInfo/totalListAll.do`;
+        const resultData = await axiosFetch(url, { useAt: "Y" });
+        console.log(resultData, "resultData");
+        setSendDataTable(resultData);
+    };
 
     return (
         <>
@@ -102,14 +181,21 @@ function OrderMgmt() {
             <div className="table-buttons">
                 <PopupButton targetUrl={URL.LaborPreCostDoc} data={{ label: "사전원가서", projectInfo }} />
                 <AddButton label={"추가"} onClick={() => setIsOpenAdd(true)} />
-                <ModButton label={"수정"} onClick={modifyToServer} />
+                <ModButton label={"수정"} onClick={() => setIsOpenUpDate(true)} />
                 <DelButton label={"삭제"} onClick={deleteToServer} />
                 <RefreshButton onClick={refresh} />
             </div>
-            <ReactDataTable columns={columns} suffixUrl="/baseInfrm/product/pjOrdrInfo" tableRef={orderMgmtTable} viewPageName="수주등록관리" />
-            {
-                isOpenAdd && <AddModal width="300" height="500" columns={addColumns} onClose={() => setIsOpenAdd(false)} sendData={addToServer}/>
-            }
+            <ReactDataTable
+                columns={columns}
+                sendData={sendDataTable}
+                suffixUrl="/baseInfrm/product/pjOrdrInfo"
+                tableRef={orderMgmtTable}
+                viewPageName="수주등록관리"
+                saveIdNm={saveIdNm}
+            />
+            {isOpenAdd && <AddModal width="300" height="500" columns={addColumns} sendData={addToServer} onClose={() => setIsOpenAdd(false)} />}
+            {isOpenAdd && <AddModal width="300" height="500" columns={addColumns} sendData={modifyToServer} onClose={() => setIsOpenUpDate(false)} />}
+            {/*<DeleteModal viewName={poiNm} onConfirm={deleteToServer} />*/}
         </>
     );
 }
